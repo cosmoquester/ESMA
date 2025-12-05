327f89a261545928dd4c4d0b7aa46d06a1768cd0
Sangjun Park <cosmoquester@gmail.com>
2025-12-05 01:20:50 -0600

    feat: Add script to eval with best treshold

--- scripts/eval_trivia_qa_threshold.py
+++ scripts/eval_trivia_qa_threshold.py
@@ -2,6 +2,7 @@ import argparse
 import csv
 import os
 
+import torch
 from torch.utils.data import DataLoader
 from tqdm import tqdm
 from transformers import AutoModelForCausalLM, AutoTokenizer
@@ -11,14 +12,26 @@ from meta.dataset import RLDataset, pad_collate_fn
 from meta.metric import IGNORE_VALUE, meta_metrics
 from meta.utils import get_logger, seed_everything
 
+torch.set_grad_enabled(False)
+
 parser = argparse.ArgumentParser(description="Evaluate LLM on TriviaQA and save to TSV")
-parser.add_argument("--model", type=str, default="Qwen/Qwen2.5-0.5B-Instruct", help="HuggingFace Model ID")
+parser.add_argument(
+    "--model",
+    type=str,
+    default="Qwen/Qwen2.5-0.5B-Instruct",
+    help="HuggingFace Model ID",
+)
 parser.add_argument("--split", type=str, default="validation", help="Split to evaluate")
 parser.add_argument("--batch-size", type=int, default=128, help="Batch size for inference")
 parser.add_argument("--num-samples", type=int, help="Number of samples to evaluate (0 for all)")
 parser.add_argument("--output-path", type=str, help="Output TSV file path")
 parser.add_argument("--max-input-length", type=int, default=128, help="Maximum length of the input text")
-parser.add_argument("--max-output-length", type=int, default=32, help="Maximum length of the output text")
+parser.add_argument(
+    "--max-output-length",
+    type=int,
+    default=32,
+    help="Maximum length of the output text",
+)
 parser.add_argument("--num-workers", type=int, default=os.cpu_count() // 2, help="Number of workers")
 parser.add_argument("--seed", type=int, default=42, help="Random seed")
 
@@ -31,6 +44,8 @@ def main(args):
 
     logger.info(f"[+] Loading model: {args.model}")
     tokenizer = AutoTokenizer.from_pretrained(args.model, padding_side="left")
+    yes_token_id = tokenizer.vocab["Yes"]
+    no_token_id = tokenizer.vocab["No"]
 
     if tokenizer.pad_token is None:
         tokenizer.pad_token = tokenizer.eos_token
@@ -42,14 +57,18 @@ def main(args):
     data = load_trivia_qa_rl(split=args.split, num_samples=args.num_samples)
     dataset = RLDataset(data, tokenizer, max_length=args.max_input_length, use_meta=True)
     data_loader = DataLoader(
-        dataset, batch_size=args.batch_size, shuffle=False, num_workers=args.num_workers, collate_fn=pad_collate_fn
+        dataset,
+        batch_size=args.batch_size,
+        shuffle=False,
+        num_workers=args.num_workers,
+        collate_fn=pad_collate_fn,
     )
     logger.info(f"[+] Total samples to evaluate: {len(data)}")
 
     if args.output_path is None:
         base_model = args.model.split("/")[-1]
         os.makedirs("eval_outputs", exist_ok=True)
-        args.output_path = f"eval_outputs/triviaqa_{base_model}_{args.split}_{args.num_samples}.tsv"
+        args.output_path = f"eval_outputs/triviaqa_{base_model}_{args.split}_{args.num_samples}_threshold.tsv"
 
     all_question_ids = []
     all_questions = []
@@ -57,6 +76,7 @@ def main(args):
     all_predictions = []
     all_meta_answers = []
     all_direct_correctness = []
+    all_yes_over_no = []
     all_yes = []
     all_yes_failures = []
     all_no_failures = []
@@ -75,16 +95,19 @@ def main(args):
 
         meta_input_ids = batch["meta_input_ids"].to(model.device)
         meta_attention_mask = batch["meta_attention_mask"].to(model.device)
-        meta_outputs = model.generate(
-            input_ids=meta_input_ids,
-            attention_mask=meta_attention_mask,
-            max_new_tokens=args.max_output_length,
-        )
-        meta_generated_tokens = meta_outputs[:, meta_input_ids.shape[1] :]
-        meta_decoded_outputs = tokenizer.batch_decode(meta_generated_tokens, skip_special_tokens=True)
+
+        model_outputs = model(input_ids=meta_input_ids, attention_mask=meta_attention_mask, logits_to_keep=1)
+        last_token_logits = model_outputs.logits[:, -1, :]
+        yes_logits = last_token_logits[:, yes_token_id]
+        no_logits = last_token_logits[:, no_token_id]
+        yes_over_no = (yes_logits - no_logits).exp().cpu().tolist()
+        meta_decoded_outputs = ["Yes" if p > 1 else "No" for p in yes_over_no]
 
         direct_correctness, yes, yes_failures, no_failures, meta_alignments = meta_metrics(
-            decoded_outputs, meta_decoded_outputs, batch["answers"], keep_length=True
+            decoded_outputs,
+            meta_decoded_outputs,
+            batch["answers"],
+            keep_length=True,
         )
 
         all_question_ids.extend(batch["question_id"])
@@ -93,10 +116,32 @@ def main(args):
         all_predictions.extend(decoded_outputs)
         all_meta_answers.extend(meta_decoded_outputs)
         all_direct_correctness.extend(direct_correctness)
+        all_yes_over_no.extend(yes_over_no)
         all_yes.extend(yes)
         all_yes_failures.extend(yes_failures)
         all_no_failures.extend(no_failures)
         all_meta_alignments.extend(meta_alignments)
+
+    sorted_results = sorted(zip(all_yes_over_no, all_direct_correctness))
+    best_threshold = sorted_results[0][0]
+    gain = 0
+    for yes_over_no, direct_correctness in sorted_results:
+        if direct_correctness:
+            if gain >= 0:
+                best_threshold = yes_over_no
+                gain = -1
+            else:
+                gain -= 1
+        else:
+            gain += 1
+    if gain > 0:
+        best_threshold = sorted_results[-1][0] + 1e-6
+    logger.info(f"[+] Best threshold: {best_threshold}")
+
+    best_meta_answer = ["Yes" if p >= best_threshold else "No" for p in all_yes_over_no]
+    _, best_yes, best_yes_failures, best_no_failures, best_meta_alignments = meta_metrics(
+        all_predictions, best_meta_answer, all_ground_truths, keep_length=True
+    )
 
     with open(args.output_path, mode="w", encoding="utf-8") as f:
         writer = csv.writer(f, delimiter="\t")
@@ -112,6 +157,12 @@ def main(args):
                 "yes_failures",
                 "no_failures",
                 "meta_alignments",
+                "yes_over_no",
+                "best_meta_answer",
+                "best_yes",
+                "best_yes_failures",
+                "best_no_failures",
+                "best_meta_alignments",
             ]
         )
         for (
@@ -125,6 +176,12 @@ def main(args):
             yes_failures,
             no_failures,
             meta_alignments,
+            yes_over_no,
+            _best_meta_answer,
+            _best_yes,
+            best_yes_failure,
+            best_no_failure,
+            best_meta_alignment,
         ) in zip(
             all_question_ids,
             all_questions,
@@ -136,6 +193,12 @@ def main(args):
             all_yes_failures,
             all_no_failures,
             all_meta_alignments,
+            all_yes_over_no,
+            best_meta_answer,
+            best_yes,
+            best_yes_failures,
+            best_no_failures,
+            best_meta_alignments,
         ):
             writer.writerow(
                 [
@@ -149,6 +212,12 @@ def main(args):
                     yes_failures,
                     no_failures,
                     meta_alignments,
+                    yes_over_no,
+                    _best_meta_answer,
+                    _best_yes,
+                    best_yes_failure,
+                    best_no_failure,
+                    best_meta_alignment,
                 ]
             )
     logger.info(f"[+] Results saved to: {args.output_path}")
@@ -166,6 +235,20 @@ def main(args):
         logger.info(f"[+] No failures rate: {sum(all_no_failures) / len(all_no_failures):.2%}")
     else:
         logger.info("[-] All meta answers are Yes")
+
+    best_yes_failures = [v for v in best_yes_failures if v != IGNORE_VALUE]
+    best_no_failures = [v for v in best_no_failures if v != IGNORE_VALUE]
+    if len(best_yes_failures) > 0:
+        logger.info(f"[+] Best yes failures rate: {sum(best_yes_failures) / len(best_yes_failures):.2%}")
+    else:
+        logger.info("[-] All best meta answers are No")
+    if len(best_no_failures) > 0:
+        logger.info(f"[+] Best no failures rate: {sum(best_no_failures) / len(best_no_failures):.2%}")
+    else:
+        logger.info("[-] All best meta answers are Yes")
+    logger.info(f"[+] Best meta alignments: {sum(best_meta_alignments) / len(best_meta_alignments):.2%}")
+    logger.info(f"[+] Best yes rate: {sum(best_yes) / len(best_yes):.2%}")
+    logger.info(f"[+] Best meta alignments: {sum(best_meta_alignments) / len(best_meta_alignments):.2%}")
 
 
 if __name__ == "__main__":
