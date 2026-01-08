cabce95a9c4b1255830099a330c9f3eb0fb692a1
Sangjun Park <cosmoquester@gmail.com>
2026-01-07 23:40:13 -0600

    feat: support weight change from bottom

--- scripts/apply_weight_change.py
+++ scripts/apply_weight_change.py
@@ -8,7 +8,10 @@ from transformers import AutoModelForCausalLM, AutoTokenizer
 parser = argparse.ArgumentParser()
 parser.add_argument("model", type=str)
 parser.add_argument("weight_change", type=str)
-parser.add_argument("--filter-highest", type=float, default=1.0)
+parser.add_argument(
+    "--filter-ratio", type=float, default=1.0, help="Ratio of weight change to filter, 1.0 means no filtering"
+)
+parser.add_argument("--from-bottom", "-b", action="store_true", help="Filter from bottom instead of top")
 parser.add_argument("--output-path", "-o", type=str, required=True)
 args = parser.parse_args()
 
@@ -22,8 +25,8 @@ def main(args):
     with open(args.weight_change, "rb") as f:
         weight_change = pickle.load(f)
 
-    if args.filter_highest < 1.0:
-        print(f"[+] Filtering highest {args.filter_highest * 100}% of weight change...")
+    if args.filter_ratio < 1.0:
+        print(f"[+] Filtering highest {args.filter_ratio * 100}% of weight change...")
         # Collect all absolute values efficiently
         abs_values = []
         for v in tqdm(weight_change.values(), desc="Filtering weight change"):
@@ -32,7 +35,7 @@ def main(args):
         print(f"[+] Concatenating {len(abs_values)} values...")
         all_values = torch.cat(abs_values)
 
-        k = int(len(all_values) * (1 - args.filter_highest))
+        k = int(len(all_values) * (1 - args.filter_ratio))
         if k == 0:
             k = 1
         print(f"[+] Finding {k}-th smallest value (out of {len(all_values)})...")
@@ -45,8 +48,11 @@ def main(args):
     for name, param in tqdm(model.named_parameters(), desc="Applying weight change"):
         if name in weight_change:
             v = weight_change[name]
-            if args.filter_highest < 1.0:
-                v[v.abs() < threshold] = 0
+            if args.filter_ratio < 1.0:
+                mask = v.abs() < threshold
+                if args.from_bottom:
+                    mask = ~mask
+                v[mask] = 0
             param.data.add_(v)
 
     print("[+] Saving model...")
