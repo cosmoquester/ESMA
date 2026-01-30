ae94d092b3bc7f9ddbab06ca9bf3eb5346693748
Sangjun Park <cosmoquester@gmail.com>
2026-01-30 08:40:44 -0600

    fix: remove unused rewards

--- meta/reward.py
+++ meta/reward.py
@@ -1,6 +1,3 @@
-from .metric import type2_d_prime
-
-
 def correct_reward(direct_correctness: list[int], meta_yes: list[int]) -> list[int]:
     return direct_correctness.copy()
 
@@ -9,39 +6,7 @@ def meta_alignment_reward(direct_correctness: list[int], meta_yes: list[int]) ->
     return [int(correct == yes) for correct, yes in zip(direct_correctness, meta_yes)]
 
 
-def multilevel_reward(direct_correctness: list[int], meta_yes: list[int]) -> list[int]:
-    rewards = []
-    for correct, yes in zip(direct_correctness, meta_yes):
-        if correct == yes:
-            if correct:
-                rewards.append(3)
-            else:
-                rewards.append(2)
-        else:
-            if correct:
-                rewards.append(1)
-            else:
-                rewards.append(0)
-    return rewards
-
-
-def multilevel_reward2(direct_correctness: list[int], meta_yes: list[int]) -> list[int]:
-    rewards = []
-    for correct, yes in zip(direct_correctness, meta_yes):
-        if correct == yes:
-            if correct:
-                rewards.append(4)
-            else:
-                rewards.append(2)
-        else:
-            if correct:
-                rewards.append(1)
-            else:
-                rewards.append(0)
-    return rewards
-
-
-def multilevel_reward3(direct_correctness: list[int], meta_yes: list[int]) -> list[int]:
+def esma_reward(direct_correctness: list[int], meta_yes: list[int]) -> list[int]:
     rewards = []
     for correct, yes in zip(direct_correctness, meta_yes):
         if correct == yes:
@@ -55,51 +20,10 @@ def multilevel_reward3(direct_correctness: list[int], meta_yes: list[int]) -> li
             else:
                 rewards.append(0)
     return rewards
-
-
-def multilevel_reward4(direct_correctness: list[int], meta_yes: list[int]) -> list[int]:
-    rewards = []
-    for correct, yes in zip(direct_correctness, meta_yes):
-        if correct == yes:
-            if correct:
-                rewards.append(2)
-            else:
-                rewards.append(1)
-        else:
-            if correct:
-                rewards.append(0)
-            else:
-                rewards.append(0)
-    return rewards
-
-
-def multilevel_reward5(direct_correctness: list[int], meta_yes: list[int]) -> list[int]:
-    rewards = []
-    for correct, yes in zip(direct_correctness, meta_yes):
-        if correct == yes:
-            if correct:
-                rewards.append(5)
-            else:
-                rewards.append(2)
-        else:
-            if correct:
-                rewards.append(0)
-            else:
-                rewards.append(-10)
-    return rewards
-
-
-def type2_d_prime_reward(direct_correctness: list[int], meta_yes: list[int]) -> list[float]:
-    return [type2_d_prime(direct_correctness, meta_yes)]
 
 
 REWARD_TYPE_TO_FUNCTION = {
     "correct": correct_reward,
     "alignment": meta_alignment_reward,
-    "multilevel": multilevel_reward,
-    "multilevel2": multilevel_reward2,
-    "multilevel3": multilevel_reward3,
-    "multilevel4": multilevel_reward4,
-    "multilevel5": multilevel_reward5,
-    "type2_d_prime": type2_d_prime_reward,
+    "esma": esma_reward,
 }
